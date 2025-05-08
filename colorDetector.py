be44b207e022236062c4495f95adcaa726ea33ea
Blitz <jsnajera1017@gmail.com>
2025-05-08 04:22:21 -0600

    Created Color classifier

--- colorDetector.py
+++ colorDetector.py
@@ -4,26 +4,26 @@ import numpy as np
 
 def detectColor(figure):
 
-    # Convierte a espacio de color HSV
+    #HSV scale convertor
     hsv = cv2.cvtColor(figure, cv2.COLOR_BGR2HSV)
 
-    # Máscara para rojo (dos rangos en HSV)
+    #RedMask
     lowRed1 = np.array([0, 100, 100])
     highRed1 = np.array([10, 255, 255])
     lowRed2 = np.array([160, 100, 100])
     highRed2 = np.array([179, 255, 255])
     redMask = cv2.inRange(hsv, lowRed1, highRed1) + cv2.inRange(hsv, lowRed2, highRed2)
 
-    # Máscara para negro (valores de brillo bajos)
+    #Black Mask
     lowBlack = np.array([0, 0, 0])
     highBlack = np.array([180, 255, 50])
     blackMask = cv2.inRange(hsv, lowBlack, highBlack)
 
-    # Cuenta los píxeles de cada color
+    #Counts pixels on each mask
     RedPixels = cv2.countNonZero(redMask)
     blackPixels = cv2.countNonZero(blackMask)
 
-    # Resultado
+    #Returns 1 if figure is red, otherwise return black
     if RedPixels > blackPixels:
         return 1
     else:
