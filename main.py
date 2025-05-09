175db03cd099504c860448fba5ca8a387851f444
EloyRb0 <eloyrb@outlook.es>
2025-05-08 20:22:50 -0600

    Hands are now compared from 2 players, and a winner is drawn

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import cardDetector
 import handDetector
+import winDetector
 
 # ---- Main Execution ----
-table_cards = cardDetector.process_image('PokerTest.jpeg', 5)     # Public cards
-player_cards = cardDetector.process_image('player2.jpeg', 2)   # Player’s hand
-all_cards = table_cards + player_cards
+table_cards_1 = cardDetector.process_image('PokerTest.jpeg', 5)     # Insert table photo
+player_cards_1 = cardDetector.process_image('player2.jpeg', 2)   # Insert Player 1 hand
+all_cards_1 = table_cards_1 + player_cards_1
 
-best_hand = handDetector.detect_hand(all_cards)
-print(f"Best Hand: {best_hand}")
+best_hand_1 = handDetector.detect_hand(all_cards_1)
+print(f"Best Hand Player 1: {best_hand_1}")
+
+table_cards_2 = cardDetector.process_image('PokerTest.jpeg', 5)     # Insert table photo as well
+player_cards_2 = cardDetector.process_image('player2.jpeg', 2)   # Insert Player 2 hand
+all_cards_2 = table_cards_2 + player_cards_2
+
+best_hand_2 = handDetector.detect_hand(all_cards_2)
+print(f"Best Hand Player 2: {best_hand_2}")
+
+winner = winDetector.compare_hands(best_hand_1, best_hand_2)
+print(f"Winner: {winner}")
+
+
+
+
